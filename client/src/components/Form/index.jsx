ac923f412056e46e3305e4bf399ed76f887eb2bd
Leila <leiisalguero@gmaill.com>
2025-01-08 16:45:23 -0300

    delete function

--- client/src/components/Form/index.jsx
+++ client/src/components/Form/index.jsx
@@ -10,19 +10,6 @@ const Form = ({ form, handleChange, handleSubmit }) => {
             : createPeople(form)
     }
 
-
-    const thereAreEmptyValues = () => {
-        return (
-            !form.name ||
-            !form.lastname ||
-            !form.gender ||
-            !form.birthdate ||
-            !form.email ||
-            !form.phone ||
-            !form.maritalStatus
-        )
-    }
-
     return (
         <form className={styles.form} onSubmit={handleSubmit}>
             {formFields.map((field, index) => {
@@ -56,7 +43,7 @@ const Form = ({ form, handleChange, handleSubmit }) => {
                     );
                 }
             })}
-            <button onClick={handleClick} type="submit" >
+            <button onClick={handleClick} type="submit">
                 {form.id ? "Update Person" : "Add Person"}
             </button>
         </form>
