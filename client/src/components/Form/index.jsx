3f0eb81e4f03dfeb985055fccaed93b23414ea4f
Leila <leiisalguero@gmaill.com>
2025-01-08 15:13:31 -0300

    add client folder with react proyect

---
+++ client/src/components/Form/index.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from "./form.module.css"
+import { formFields } from "../constants";
+import { updatePeople, createPeople } from "./../../apiService"
+
+const Form = ({ form, handleChange, handleSubmit }) => {
+    const handleClick = () => {
+        return form.id
+            ? updatePeople(form.id, form)
+            : createPeople(form)
+    }
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit}>
+            {formFields.map((field, index) => {
+                if (field.type === "select") {
+                    return (
+                        <select
+                            key={index}
+                            name={field.name}
+                            value={form[field.name] || ""}
+                            onChange={handleChange}
+                            required={field.required}
+                        >
+                            {field.options.map((option, idx) => (
+                                <option key={idx} value={option.value}>
+                                    {option.label}
+                                </option>
+                            ))}
+                        </select>
+                    );
+                } else {
+                    return (
+                        <input
+                            key={index}
+                            type={field.type}
+                            name={field.name}
+                            value={form[field.name]}
+                            onChange={handleChange}
+                            placeholder={field.placeholder}
+                            required={field.required}
+                        />
+                    );
+                }
+            })}
+            <button onClick={handleClick} type="submit">
+                {form.id ? "Update Person" : "Add Person"}
+            </button>
+        </form>
+    )
+}
+
+export default Form;
---
+++ client/src/components/constants.js
@@ -0,0 +1,52 @@
+export const formFields = [
+    {
+        type: "text",
+        name: "name",
+        placeholder: "First Name",
+        required: true,
+    },
+    {
+        type: "text",
+        name: "lastname",
+        placeholder: "Last Name",
+        required: true,
+    },
+    {
+        type: "select",
+        name: "gender",
+        options: [
+            { value: "", label: "Select Gender" },
+            { value: "Male", label: "Male" },
+            { value: "Female", label: "Female" },
+            { value: "Other", label: "Otro" },
+        ],
+        required: true,
+    },
+    {
+        type: "date",
+        name: "birthdate",
+        required: true,
+    },
+    {
+        type: "email",
+        name: "email",
+        placeholder: "Email",
+        required: true,
+    },
+    {
+        type: "text",
+        name: "phone",
+        placeholder: "Phone",
+        required: true,
+    },
+    {
+        type: "select",
+        name: "maritalStatus",
+        options: [
+            { value: "", label: "Marital Status" },
+            { value: "Single", label: "Single" },
+            { value: "Married", label: "Married" },
+        ],
+        required: true,
+    },
+];
